2eed7c81cfa1d271a19c97576a1783734a362cdd
Rasmus Nilsson <Rasmus Nilsson>
2017-03-27 14:50:22 +0200

    git test

--- app/src/main/java/sportapp/pvt_sportapp/MainActivity.java
+++ app/src/main/java/sportapp/pvt_sportapp/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) { //adrian test Anton merge
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //GIT test
     }
 }
