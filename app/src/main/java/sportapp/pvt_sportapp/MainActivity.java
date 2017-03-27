19b52254c452776f75304d08181c45989fa97081
Fluchey <anton@fluch.se>
2017-03-27 14:56:46 +0200

    Fixat git-manual

--- app/src/main/java/sportapp/pvt_sportapp/MainActivity.java
+++ app/src/main/java/sportapp/pvt_sportapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) { //adrian test Anton merge
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
