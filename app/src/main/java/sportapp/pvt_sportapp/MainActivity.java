281f001d22e20e6aee4140b99fd4467bba6aa4f9
Fluchey <anton@fluch.se>
2017-03-27 15:36:12 +0200

    Lagt till registerskärm och kopplat register here till den skärmen

--- app/src/main/java/sportapp/pvt_sportapp/MainActivity.java
+++ app/src/main/java/sportapp/pvt_sportapp/MainActivity.java
@@ -1,14 +1,26 @@
 package sportapp.pvt_sportapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) { //adrian test Anton merge Maja Merge
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //GIT test
+
+        TextView tvLoginRegisterHere = (TextView) findViewById(R.id.tvLoginRegisterHere);
+
+        tvLoginRegisterHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                MainActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
