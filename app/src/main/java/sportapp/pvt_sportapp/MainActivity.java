f14ebcaf34caa63bcd99841522b92ce565f416de
AdBergman <peradrianbergman@eduroam-77-238-55-193.public.su.se>
2017-03-28 13:58:29 +0200

    facebook integration main_activity.java unchanged

--- app/src/main/java/sportapp/pvt_sportapp/MainActivity.java
+++ app/src/main/java/sportapp/pvt_sportapp/MainActivity.java
@@ -1,26 +1,16 @@
 package sportapp.pvt_sportapp;
 
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-
-public class MainActivity extends AppCompatActivity implements LayoutInflater {
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState, LayoutInflater inflater, ViewGroup container) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        CallbackManager callbackManager;
         setContentView(R.layout.activity_main);
 
         TextView tvLoginRegisterHere = (TextView) findViewById(R.id.tvLoginRegisterHere);
@@ -32,36 +22,5 @@ public class MainActivity extends AppCompatActivity implements LayoutInflater {
                 MainActivity.this.startActivity(registerIntent);
             }
         });
-
-        View view = inflater.inflate(R.layout.null, container, false); //layout.splash
-        callbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = (LoginButton) view.findViewById(R.id.login_button;
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() { ... });
-
-        loginButton = (LoginButton) view.findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email");
-        // If using in a fragment
-        loginButton.setFragment(this);
-        // Other app specific specialization
-
-        // Callback registration
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                // App code
-            }
-
-            @Override
-            public void onCancel() {
-                // App code
-            }
-
-            @Override
-            public void onError(FacebookException exception) {
-                // App code
-            }
-
     }
-
-
 }
