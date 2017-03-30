e7a22e56db99f6da94aedbafde615d0df1773b74
Fluchey <anton@fluch.se>
2017-03-30 14:59:25 +0200

    Added correct number of fields in activity_register.xml, started working on JSON object wehn postin to API

--- app/src/main/java/sportapp/pvt_sportapp/RegisterActivity.java
+++ app/src/main/java/sportapp/pvt_sportapp/RegisterActivity.java
@@ -1,10 +1,5 @@
 package sportapp.pvt_sportapp;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -12,20 +7,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -36,26 +21,39 @@ public class RegisterActivity extends AppCompatActivity {
 
         Button signUpButton = (Button) findViewById(R.id.registerButton);
 
-        signUpButton.setOnClickListener(new View.OnClickListener(){
+        signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText username = (EditText) findViewById(R.id.etRegisterUsername);
                 EditText password = (EditText) findViewById(R.id.etRegisterPassword);
-                EditText email = (EditText) findViewById(R.id.etRegisterEmail);
+                EditText firstName = (EditText) findViewById(R.id.etRegisterFirstName);
+                EditText lastName = (EditText) findViewById(R.id.etRegisterLastName);
+                EditText phoneNumber = (EditText) findViewById(R.id.etRegisterPhoneNumber);
+                EditText email = (EditText) findViewById(R.id.etRegisterMail);
                 TextView message = (TextView) findViewById(R.id.tvSignUpMessage);
 
                 if (TextUtils.isEmpty(username.getText().toString())) {
                     message.setText("Invalid Username, try again");
-                }else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
+                } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
                     message.setText("Invalid Email, try again");
-                }else if (TextUtils.isEmpty(password.getText().toString())) {
+                } else if (TextUtils.isEmpty(password.getText().toString())) {
                     message.setText("Invalid Password, try again");
+                } else {
+                    JSONObject jsonObject = new JSONObject();
+                    try {
+                        jsonObject.put("user_id",username.getText().toString());
+                        jsonObject.put("firstname", firstName.getText().toString());
+                        jsonObject.put("lastname", lastName.getText().toString());
+                        jsonObject.put("mobilnummer", phoneNumber.getText().toString());
+                        jsonObject.put("user_mail", email.getText().toString());
+                        jsonObject.put("password_user", password.getText().toString());
+                        Log.d("JsonObject", jsonObject.toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
-                //else{
-                // skapa användare, pusha till databas
-                // hoppa vidare till någon slags "profil"}
             }
-        } );
+        });
     }
 }
 
