063f13098067271003c383875032de8de77c6d28
Fluchey <anton@fluch.se>
2017-03-31 12:10:01 +0200

    Added user area activity and Toasts to great the user after account creation

--- app/src/main/java/sportapp/pvt_sportapp/RegisterActivity.java
+++ app/src/main/java/sportapp/pvt_sportapp/RegisterActivity.java
@@ -1,6 +1,8 @@
 package sportapp.pvt_sportapp;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,15 +12,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class RegisterActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,7 @@ public class RegisterActivity extends AppCompatActivity {
         } else if (TextUtils.isEmpty(password.getText().toString())) {
             message.setText("Invalid Password, try again");
         } else {
+            message.setText("");
             JSONObject jsonObject = new JSONObject();
             try {
                 jsonObject.put("user_id", username.getText().toString());
@@ -55,11 +62,25 @@ public class RegisterActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
             /* Here the asynctask is triggered */
-            new RegisterRequest().execute(jsonObject.toString());
+            new RegisterRequest(this).execute(jsonObject.toString());
         }
     }
 
-    private class RegisterRequest extends AsyncTask<String, Void, Void> {
+    private class RegisterRequest extends AsyncTask<String, RegisterActivity, Void> {
+        private ProgressDialog dialog;
+        private RegisterActivity ra;
+        protected int responseCode;
+        protected String responseBody;
+        public RegisterRequest(RegisterActivity registerActivity) {
+            ra =registerActivity;
+            dialog = new ProgressDialog(registerActivity);
+        }
+        @Override
+        protected void onPreExecute(){
+            dialog.setMessage("Creating account, please wait");
+            dialog.show();
+        }
+
         @Override
         protected Void doInBackground(String... params) {
             try {
@@ -74,10 +95,41 @@ public class RegisterActivity extends AppCompatActivity {
                 osw.flush();
                 osw.close();
                 Log.d("Responsecode", connection.getResponseCode() + "");
+                BufferedReader br;
+                if (200 <= connection.getResponseCode() && connection.getResponseCode() <= 299) {
+                    br = new BufferedReader(new InputStreamReader((connection.getInputStream())));
+                } else {
+                    br = new BufferedReader(new InputStreamReader((connection.getErrorStream())));
+                }
+
+                StringBuilder out = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    out.append(line);
+                }
+                Log.d("JSON?", out.toString());
+                JSONObject jsonObject = new JSONObject(out.toString());
+                responseBody = jsonObject.getString("body");
+                responseCode = connection.getResponseCode();
             } catch (Exception e) {
                 e.printStackTrace();
             }
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            if(dialog.isShowing()){
+                dialog.dismiss();
+            }
+
+            /* Responsecode 201 maps to successfull account creation */
+            if(responseCode == 201){
+                Toast.makeText(ra, "Account created!", Toast.LENGTH_LONG).show();
+            }else {
+                Toast.makeText(ra, responseBody, Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
