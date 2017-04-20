56749e34d38a1708725a1f0cd9d32de63374dba5
fluchey <anton.fluch@gmail.com>
2017-04-20 13:03:39 +0200

    Small fixes in user area

--- app/src/main/java/com/sportify/userArea/activity/UserAreaActivity.java
+++ app/src/main/java/com/sportify/userArea/activity/UserAreaActivity.java
@@ -2,19 +2,31 @@ package com.sportify.userArea.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+
+import com.sportify.userArea.presenter.UserAreaPresenter;
+import com.sportify.userArea.presenter.UserAreaPresenterImpl;
 
 import sportapp.pvt_sportapp.R;
 
 public class UserAreaActivity extends AppCompatActivity implements UserAreaView {
+    private UserAreaPresenter userAreaPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
+
+        userAreaPresenter = new UserAreaPresenterImpl();
     }
 
-    public void findEventButtonClick(){
-        Toast.makeText(this, "I do nothing, ask my developers why", Toast.LENGTH_LONG);
+
+    public void findEventButtonClick(View v) {
+        Toast.makeText(this, "I do nothing, ask my developers why", Toast.LENGTH_LONG).show();
+    }
+
+    public void createEventButtonClick(View v) {
+        Toast.makeText(this, "I do nothing, ask my developers why", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/sportify/userArea/activity/UserAreaView.java
+++ app/src/main/java/com/sportify/userArea/activity/UserAreaView.java
@@ -1,9 +1,12 @@
 package com.sportify.userArea.activity;
 
+import android.view.View;
+
 /**
  * Created by fluchey on 2017-04-20.
  */
 
 public interface UserAreaView {
+
 
 }
