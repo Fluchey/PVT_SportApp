d46574ede24abf0bb561aab08f2c05332eb263bc
AdBergman <bergman.adrian@gmail.com>
2017-04-26 15:31:14 +0200

    changed LoginRequestImpl to accept dynamic url

--- app/src/main/java/com/sportify/login/presenter/LoginPresenter.java
+++ app/src/main/java/com/sportify/login/presenter/LoginPresenter.java
@@ -1,10 +1,12 @@
 package com.sportify.login.presenter;
 
+import com.facebook.AccessToken;
+
 /**
  * Created by peradrianbergman on 2017-04-18.
  */
 
 public interface LoginPresenter {
     void loginUser();
-    void loginFB();
+    void requestFacebookLong(AccessToken token);
 }
