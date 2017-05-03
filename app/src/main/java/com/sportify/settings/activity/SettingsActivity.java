ea2781e0ec92b464dd9e693c2e289223e1a8c1bc
Carolin <carolintran91@gmail.com>
2017-05-03 21:52:50 +0200

    Ny sida: inställningar, skapat mapp och view. Ändrat: färg, theme.

---
+++ app/src/main/java/com/sportify/settings/activity/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.sportify.settings.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sportapp.pvt_sportapp.R;
+
+public class SettingsActivity extends AppCompatActivity implements SettingsView{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
---
+++ app/src/main/java/com/sportify/settings/activity/SettingsView.java
@@ -0,0 +1,5 @@
+package com.sportify.settings.activity;
+
+public interface SettingsView {
+
+}
