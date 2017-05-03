b647592dff2d79bb8167a6f87107c9d86607800a
Carolin <carolintran91@gmail.com>
2017-05-03 21:54:19 +0200

    Ny sida: glömt lösenord. Små ändringar i mainPage och login.

--- app/src/main/java/com/sportify/forgottenPass/activity/ForgottenPassActivity.java
+++ app/src/main/java/com/sportify/forgottenPass/activity/ForgottenPassActivity.java
@@ -1,15 +1,15 @@
-package com.sportify.login.activity;
+package com.sportify.forgottenPass.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import sportapp.pvt_sportapp.R;
 
-public class ForgottenPasswActivity extends AppCompatActivity {
+public class ForgottenPassActivity extends AppCompatActivity implements ForgottenPassView{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forgotten_passw);
+        setContentView(R.layout.activity_forgotten_pass);
     }
 }
---
+++ app/src/main/java/com/sportify/forgottenPass/activity/ForgottenPassView.java
@@ -0,0 +1,5 @@
+package com.sportify.forgottenPass.activity;
+
+public interface ForgottenPassView {
+
+}
