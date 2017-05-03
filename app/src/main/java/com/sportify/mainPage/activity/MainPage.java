dd764d9f60258830a1e635e18def0a50e5d00312
Carolin <carolintran91@gmail.com>
2017-05-03 21:53:44 +0200

    Flyttat på MainPage så att det är i egen mapp.

--- app/src/main/java/com/sportify/mainPage/activity/MainPage.java
+++ app/src/main/java/com/sportify/mainPage/activity/MainPage.java
@@ -1,4 +1,4 @@
-package com.sportify;
+package com.sportify.mainPage.activity;
 
 
 import android.support.v7.app.AppCompatActivity;
---
+++ app/src/main/java/com/sportify/mainPage/activity/MainPageView.java
@@ -0,0 +1,5 @@
+package com.sportify.mainPage.activity;
+
+public interface MainPageView {
+
+}
