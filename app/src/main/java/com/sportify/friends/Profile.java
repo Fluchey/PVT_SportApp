7f2c757639e8b2f273314f00b5db488264396806
Maja <maja.lund@live.se>
2017-05-04 10:25:52 +0200

    Test

---
+++ app/src/main/java/com/sportify/friends/Profile.java
@@ -0,0 +1,33 @@
+package com.sportify.friends;
+
+/**
+ * Created by Maja on 2017-05-03.
+ */
+
+
+//Test
+public class Profile {
+
+    private String firstname;
+    private String lastname;
+    private int profilePicture;
+
+    public Profile(String firstname, String lastname, int profilePicture){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.profilePicture = profilePicture;
+    }
+
+    public String getFirstname(){
+        return firstname;
+    }
+
+    public String getLastname(){
+        return lastname;
+    }
+
+    public int getProfilePicture(){
+        return profilePicture;
+    }
+
+}
