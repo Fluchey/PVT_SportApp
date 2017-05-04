f1ac92e2a15e4af87a51be6ec3fbbc47a48ab04b
Maja <maja.lund@live.se>
2017-05-04 14:58:14 +0200

    Strukturerade upp createEvent med mappar för alla fem sidor

---
+++ app/src/main/java/com/sportify/createEvent/createEventPageFive/createEventActivity.java
@@ -0,0 +1,15 @@
+package com.sportify.createEvent.createEventPageFive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sportapp.pvt_sportapp.R;
+
+public class createEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event2);
+    }
+}
