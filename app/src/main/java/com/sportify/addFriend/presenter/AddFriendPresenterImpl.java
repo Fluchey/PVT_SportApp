a0289cc3c4da0ef6539eb79c0e8ef0b944e75ac8
Maja <maja.lund@live.se>
2017-05-10 16:22:23 +0200

    worked some more with adding friends

--- app/src/main/java/com/sportify/addFriend/presenter/AddFriendPresenterImpl.java
+++ app/src/main/java/com/sportify/addFriend/presenter/AddFriendPresenterImpl.java
@@ -33,30 +33,28 @@ public class AddFriendPresenterImpl implements AddFriendPresenter, AddFriendRequ
         this.sharedPref = sharedPref;
         this.token = sharedPref.getString("jwt", "");
         this.addFriendRequest = new AddFriendRequestImpl(this, token);
-        getFriendsMakeApiRequest();
+        getAllUsersMakeApiRequest();
         System.out.println("Nu har vi kört");
     }
 
     @Override
     public void addFriend(int profileID) {
         JSONObject jsonObject = new JSONObject();
-//        int friendID = 4;
         try{
             jsonObject.put("friendID", "" + profileID);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        System.out.println("Add friend: " + jsonObject.toString());
         addFriendRequest.makeApiRequestAddFriend("PUT", "addfriend", jsonObject.toString());
     }
 
     @Override
-    public void getFriendsMakeApiRequest() {
-        addFriendRequest.makeApiRequestGetUsers("POST", "findfriends", "{}");
+    public void getAllUsersMakeApiRequest() {
+        addFriendRequest.makeApiRequestGetUsers("GET", "getusers");
     }
 
     @Override
-    public void getFriendsFromApiResponse(String jsonMessage) {
+    public void getAllUsersFromApiResponse(String jsonMessage) {
         JSONObject json = null;
         JSONArray array = null;
 
@@ -66,7 +64,7 @@ public class AddFriendPresenterImpl implements AddFriendPresenter, AddFriendRequ
 
         try {
             json = new JSONObject(jsonMessage);
-            array = json.getJSONArray("friendList");
+            array = json.getJSONArray("users");
             Log.d("JsonArr: ", array.toString());
         } catch (JSONException e) {
             e.printStackTrace();
@@ -82,8 +80,15 @@ public class AddFriendPresenterImpl implements AddFriendPresenter, AddFriendRequ
                 JSONObject jsonObject = array.getJSONObject(i);
                 //TODO: Byt ut facebook icon till profilbild
                 String firstName = jsonObject.getString("firstname");
-                String lastName = jsonObject.getString("lastname");
                 int profileID = jsonObject.getInt("profileID");
+                String lastName = jsonObject.getString("lastname");
+
+                if(firstName.isEmpty()){
+                    firstName = "Anonym";
+                }
+                if(lastName.isEmpty()){
+                    lastName = " ";
+                }
 
                 Profile friend = new Profile(firstName, lastName, R.drawable.userprofileimage, profileID);
 
@@ -112,7 +117,7 @@ public class AddFriendPresenterImpl implements AddFriendPresenter, AddFriendRequ
     @Override
     public void showApiResponse(String... params) {
 
-        getFriendsFromApiResponse(params[0]);
+        getAllUsersFromApiResponse(params[0]);
         showFriends();
         updateFriendSearchView();
     }
