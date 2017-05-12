16dcc1f86b84cf9f5b58d1068e02a58e27c7aff8
Carolin <carolintran91@gmail.com>
2017-05-12 02:44:07 +0200

    Back-buttons on Forgotten pass and Register acc.

--- app/src/main/java/com/sportify/forgottenPass/activity/ForgottenPassActivity.java
+++ app/src/main/java/com/sportify/forgottenPass/activity/ForgottenPassActivity.java
@@ -1,7 +1,12 @@
 package com.sportify.forgottenPass.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.sportify.login.activity.LoginActivity;
+import com.sportify.mainPage.activity.MainPageActivity;
 
 import sportapp.pvt_sportapp.R;
 
@@ -11,5 +16,11 @@ public class ForgottenPassActivity extends AppCompatActivity implements Forgotte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgotten_pass);
+    }
+
+
+    public void toLoginFromForgottenActivity(View v){
+        Intent goToLoginViewIntent = new Intent(ForgottenPassActivity.this, LoginActivity.class);
+        ForgottenPassActivity.this.startActivity(goToLoginViewIntent);
     }
 }
