bf4efd2fcdf25a09d60434ebe7d0da94c000b9f4
fluchey <anton.fluch@gmail.com>
2017-05-17 18:17:33 +0200

    Event area started

--- app/src/main/java/com/sportify/eventArea/activity/EventAreaActivity.java
+++ app/src/main/java/com/sportify/eventArea/activity/EventAreaActivity.java
@@ -1,33 +1,96 @@
 package com.sportify.eventArea.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.sportify.eventArea.presenter.EventAreaPresenter;
+import com.sportify.eventArea.presenter.EventAreaPresenterImpl;
+import com.sportify.eventArea.request.EventAreaRequest;
 import com.sportify.settings.activity.SettingsActivity;
 import com.sportify.userArea.activity.UserAreaActivity;
 
 import sportapp.pvt_sportapp.R;
 
 public class EventAreaActivity extends AppCompatActivity implements EventAreaView{
+    private EventAreaPresenter presenter;
     private int eventId;
+    private SharedPreferences sharedPref;
 
+
+    private TextView placeName;
+    private TextView description;
+    private TextView startTime;
+    private TextView endTime;
+    private TextView price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_area);
 
+        sharedPref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        presenter = new EventAreaPresenterImpl(this, sharedPref);
+
         Bundle bundle = getIntent().getExtras();
         eventId = (bundle.getInt("eventId"));
-        Toast.makeText(this, String.valueOf(eventId), Toast.LENGTH_SHORT).show();
+        presenter.getEventFromDb(eventId);
+
+        placeName = (TextView) findViewById(R.id.tvEventAreaPlaceText);
+        startTime = (TextView) findViewById(R.id.tvEventAreaStartTime);
+        endTime = (TextView) findViewById(R.id.tvEventAreaEndTime);
+        description = (TextView) findViewById(R.id.tvEventAreaDescription);
+        price = (TextView) findViewById(R.id.tvEventAreaPrice);
     }
 
 
     public void toUserAreaFromEvent(View v){
         Intent goToUserAreaViewIntent = new Intent(EventAreaActivity.this, UserAreaActivity.class);
         EventAreaActivity.this.startActivity(goToUserAreaViewIntent);
+    }
+
+    @Override
+    public void setPlaceName(String placeName) {
+        this.placeName.setText(placeName);
+    }
+
+    @Override
+    public void setHostName(String hostName) {
+
+    }
+
+    @Override
+    public void setStartTime(String startTime) {
+        this.startTime.setText(startTime);
+    }
+
+    @Override
+    public void setEndTime(String endTime) {
+        this.endTime.setText(endTime);
+    }
+
+    @Override
+    public void setStartDate(String startDate) {
+
+    }
+
+    @Override
+    public void setEndDate(String endDate) {
+
+    }
+
+    @Override
+    public void setPrice(int price) {
+        this.price.setText(String.valueOf(price));
+    }
+
+    @Override
+    public void setDescription(String description) {
+        this.description.setText(description);
     }
 }
---
+++ app/src/main/java/com/sportify/eventArea/presenter/EventAreaPresenterImpl.java
@@ -0,0 +1,60 @@
+package com.sportify.eventArea.presenter;
+
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.sportify.eventArea.activity.EventAreaView;
+import com.sportify.eventArea.request.EventAreaRequest;
+import com.sportify.eventArea.request.EventAreaRequestImpl;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by fluchey on 2017-05-17.
+ */
+
+public class EventAreaPresenterImpl implements EventAreaPresenter, EventAreaRequest.onRequestFinishedListener {
+    private String token;
+    private EventAreaView activity;
+    private SharedPreferences sharedPreferences;
+    private EventAreaRequest request;
+
+    public EventAreaPresenterImpl(EventAreaView eventAreaActivity, SharedPreferences sharedPreferences) {
+        this.activity = eventAreaActivity;
+        this.sharedPreferences = sharedPreferences;
+        this.token = sharedPreferences.getString("jwt", "");
+        request = new EventAreaRequestImpl(this, token);
+    }
+
+
+
+    @Override
+    public void getEventFromDb(int eventId) {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("eventId", String.valueOf(eventId));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("Json", json.toString());
+        request.makeApiRequestPut(json.toString(), "event/getEventById", "PUT", "getEventById");
+    }
+
+    @Override
+    public void showApiResponse(String command, String... params) {
+        switch (command){
+            case "getEventById":
+                request.loadEventData(params[0]);
+                presentDataOnView();
+        }
+    }
+
+    private void presentDataOnView() {
+        activity.setPlaceName(request.getPlace().getName());
+        activity.setStartTime(request.getEvent().getStartTime());
+        activity.setEndTime(request.getEvent().getEndTime());
+        activity.setPrice(request.getEvent().getPrice());
+        activity.setDescription(request.getEvent().getEventDescription());
+    }
+}
