2e4c03672cd705eb71b84f6cb36c9a360fe26d90
Carolin <carolintran91@gmail.com>
2017-05-23 16:42:20 +0200

    Dublett eventAreaGuest (ingen mapp i java)

---
+++ app/src/main/java/com/sportify/eventAreaGuestActivity.java
@@ -0,0 +1,15 @@
+package com.sportify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sportapp.pvt_sportapp.R;
+
+public class eventAreaGuestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_area_guest);
+    }
+}
