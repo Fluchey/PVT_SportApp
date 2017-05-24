f33a53f132df0083eda88a726dd48d9f5da1b657
Suraj Kumar <surajkumar@Surajs-MBP.lan>
2017-05-24 16:57:12 +0200

    layout for andra anvandares profiler

---
+++ app/src/main/java/com/sportify/friendprofile/friendprofile.java
@@ -0,0 +1,15 @@
+package com.sportify.friendprofile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sportapp.pvt_sportapp.R;
+
+public class friendprofile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friendprofile);
+    }
+}
